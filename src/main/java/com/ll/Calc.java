20fa02a1c56c962942648c9ca0deef8ce73c219b
hjkim2040 <hjkim2661@gmail.com>
2023-03-15 17:32:29 +0900

    10 + (10 + 5) == 25

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -14,29 +14,14 @@ public class Calc {
         boolean needToMulti = exp.contains("*");
         boolean needToPlus = exp.contains("+") || exp.contains(" - ");
         boolean needToCompound = needToMulti && needToPlus;
-        boolean needToSplit =  exp.contains("(") || exp.contains(")");
+        boolean needToSplit = exp.contains("(") || exp.contains(")");
 
-        if ( needToSplit ) {
-            int bracketsCount = 0;
-            int splitPointIndex = -1;
+        if (needToSplit) {
+            int splitPointIndex = findSplitPointIndex(exp);
+            String firstExp = exp.substring(0, splitPointIndex);
+            String secondExp = exp.substring(splitPointIndex + 1);
 
-            for ( int i = 0; i < exp.length(); i++ ) {
-                if ( exp.charAt(i) == '(' ) {
-                    bracketsCount++;
-                }
-                else if ( exp.charAt(i) == ')' ) {
-                    bracketsCount--;
-                }
-
-                if ( bracketsCount == 0 ) {
-                    splitPointIndex = i;
-                    break;
-                }
-            }
-            String firstExp = exp.substring(0, splitPointIndex + 1);
-            String secondExp = exp.substring(splitPointIndex + 4);
-
-            char operationCode = exp.charAt(splitPointIndex + 2);
+            char operationCode = exp.charAt(splitPointIndex);
 
             exp = Calc.run(firstExp) + " " + operationCode + " " + Calc.run(secondExp);
                 return Calc.run(exp);
@@ -83,15 +68,43 @@ public class Calc {
         throw new RuntimeException("올바른 계산식이 아닙니다.");
 
     }
+    private static int findSplitPointIndexBy(String exp, char findChar) {
+        int bracketsCount = 0;
+
+        for (int i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+
+            if ( c == '(' ) {
+                bracketsCount++;
+            }
+            else if ( c == ')' ) {
+                bracketsCount--;
+            }
+            else if ( c == findChar ) {
+                if ( bracketsCount == 0 ) return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private static int findSplitPointIndex(String exp) {
+        int index = findSplitPointIndexBy(exp, '+');
+
+        if ( index >= 0 ) return index;
+
+        return findSplitPointIndexBy(exp, '*');
+    }
+
+
     private static String stripOuterBrackets(String exp) {
         int outerBracketsCount = 0;
 
-        while ( exp.charAt(outerBracketsCount) == '(' && exp.charAt(exp.length() - 1 - outerBracketsCount) == ')' ) {
-            outerBracketsCount++;
-        }
-
+        while (exp.charAt(outerBracketsCount) == '(' && exp.charAt(exp.length() - 1 - outerBracketsCount) == ')') {
+        outerBracketsCount++;}
         if ( outerBracketsCount == 0 ) return exp;
 
         return exp.substring(outerBracketsCount, exp.length() - outerBracketsCount);
     }
+
 }
